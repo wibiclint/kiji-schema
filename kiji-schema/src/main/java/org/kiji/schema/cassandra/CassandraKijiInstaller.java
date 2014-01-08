1b01a0c5f516925f217e456b99a10f498f2507b6
Clint Kelly <clint@wibidata.com>
2014-01-08 14:16:28 -0800

    Add very basic cassandra-install/-uninstall tools.
    At this point, these commands just create and delete keyspaces.

---
+++ kiji-schema/src/main/java/org/kiji/schema/cassandra/CassandraKijiInstaller.java
@@ -0,0 +1,170 @@
+/**
+ * (c) Copyright 2012 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.schema.cassandra;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+import com.google.common.base.Joiner;
+import org.apache.hadoop.conf.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.datastax.driver.core.exceptions.AlreadyExistsException;
+import com.datastax.driver.core.exceptions.QueryExecutionException;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+
+import org.kiji.schema.KijiInvalidNameException;
+import org.kiji.schema.KijiURI;
+import org.kiji.schema.KijiAlreadyExistsException;
+import org.kiji.schema.KijiNotInstalledException;
+import org.kiji.annotations.ApiAudience;
+import org.kiji.annotations.ApiStability;
+//import org.kiji.schema.impl.cassandra.CassandraMetaTable;
+//import org.kiji.schema.impl.cassandra.CassandraSchemaTable;
+//import org.kiji.schema.impl.cassandra.CassandraSystemTable;
+import org.kiji.schema.security.KijiSecurityManager;
+import org.kiji.schema.util.ResourceUtils;
+
+/** Installs or uninstalls Kiji instances from an Cassandra cluster. */
+@ApiAudience.Public
+@ApiStability.Evolving
+public final class CassandraKijiInstaller {
+  private static final Logger LOG = LoggerFactory.getLogger(CassandraKijiInstaller.class);
+
+  /** Singleton CassandraKijiInstaller. **/
+  private static final CassandraKijiInstaller SINGLETON = new CassandraKijiInstaller();
+
+  /** Constructs a CassandraKijiInstaller. */
+  private CassandraKijiInstaller() {
+  }
+
+  /**
+   * Installs the specified Kiji instance.
+   *
+   * @param uri URI of the Kiji instance to install.
+   * @param conf Hadoop configuration (TODO: Not clear if we really need this here).
+   * @throws IOException on I/O error.
+   * @throws KijiInvalidNameException if the Kiji instance name is invalid or already exists.
+   */
+  public void install(KijiURI uri, Configuration conf) throws IOException {
+    install(uri, Collections.<String, String>emptyMap(), conf);
+  }
+
+  /**
+   * Installs a Kiji instance.
+   *
+   * @param uri URI of the Kiji instance to install.
+   * @param properties Map of the initial system properties for installation, to be used in addition
+   *     to the defaults.
+   * @param conf Hadoop configuration (TODO: Not clear if we really need this here).
+   * @throws IOException on I/O error.
+   * @throws KijiInvalidNameException if the instance name is invalid or already exists.
+   */
+  public void install(
+      KijiURI uri,
+      Map<String, String> properties,
+      Configuration conf)
+      throws IOException {
+    if (uri.getInstance() == null) {
+      throw new KijiInvalidNameException(String.format(
+          "Kiji URI '%s' does not specify a Kiji instance name", uri));
+    }
+
+    // Try to create the keyspace.  If it already exists, the then the query will return an
+    // AlreadyExistsException.
+    // TODO: Figure out a cleaner way to check whether the keyspace already exists.
+
+    try {
+      // Create the keyspace for this kiji instance.
+      LOG.info(String.format("Installing Cassandra Kiji instance '%s'.", uri));
+
+      // TODO: Replace "localhost" with host from KijiURI.
+      Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+      Session cassandraSession = cluster.connect();
+
+      // TODO: Need a way for users to specify strategy, replication, etc.
+      String queryText = "CREATE KEYSPACE " + uri.getInstance() +
+          " WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 1}";
+      ResultSet results = cassandraSession.execute(queryText);
+
+      // Create a session pointing to the keyspace for the kiji instance
+      // TODO: May want to add prefix "kiji." to all Kiji keyspaces...
+
+      // Install the system, meta, and schema tables.
+      //CassandraSystemTable.install(hbaseAdmin, uri, conf, properties, tableFactory);
+      //HBaseMetaTable.install(hbaseAdmin, uri);
+      //HBaseSchemaTable.install(hbaseAdmin, uri, conf, tableFactory, lockFactory);
+
+
+    } catch (AlreadyExistsException aee) {
+      throw new KijiAlreadyExistsException(String.format(
+          "Cassandra Kiji instance '%s' already exists.", uri), uri);
+    }
+    // TODO: Security stuff
+
+    LOG.info(String.format("Installed Cassandra Kiji instance '%s'.", uri));
+  }
+
+  /**
+   * Removes a kiji instance from the Cassandra cluster including any user tables.
+   *
+   * @param uri URI of the Kiji instance to install.
+   * @param conf Hadoop configuration.
+   * @throws IOException on I/O error.
+   * @throws KijiInvalidNameException if the instance name is invalid.
+   * @throws KijiNotInstalledException if the specified instance does not exist.
+   */
+  public void uninstall(KijiURI uri, Configuration conf)
+      throws IOException {
+    if (uri.getInstance() == null) {
+      throw new KijiInvalidNameException(String.format(
+          "Kiji URI '%s' does not specify a Kiji instance name", uri));
+    }
+
+    try {
+      // Just drop the keyspace...
+      LOG.info(String.format("Removing the Cassandra Kiji instance '%s'.", uri.getInstance()));
+
+      // TODO: Replace "localhost" with host from KijiURI.
+      Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+      Session cassandraSession = cluster.connect();
+
+      String queryText = "DROP KEYSPACE " + uri.getInstance();
+      ResultSet results = cassandraSession.execute(queryText);
+    } catch (QueryExecutionException qee) {
+      // TODO: Handle this!
+    }
+
+    LOG.info(String.format("Removed Cassandra Kiji instance '%s'.", uri.getInstance()));
+  }
+
+  /**
+   * Gets an instance of a CassandraKijiInstaller.
+   *
+   * @return An instance of a CassandraKijiInstaller.
+   */
+  public static CassandraKijiInstaller get() {
+    return SINGLETON;
+  }
+}
---
+++ kiji-schema/src/main/java/org/kiji/schema/tools/CassandraInstallTool.java
@@ -0,0 +1,118 @@
+/**
+ * (c) Copyright 2012 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.schema.tools;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.kiji.annotations.ApiAudience;
+import org.kiji.common.flags.Flag;
+import org.kiji.schema.KConstants;
+import org.kiji.schema.KijiAlreadyExistsException;
+import org.kiji.schema.cassandra.CassandraKijiInstaller;
+import org.kiji.schema.KijiURI;
+import org.kiji.schema.hbase.HBaseFactory;
+import org.kiji.schema.impl.hbase.HBaseSystemTable;
+
+/**
+ * A command-line tool for installing kiji instances on hbase clusters.
+ */
+@ApiAudience.Private
+public final class CassandraInstallTool extends BaseTool {
+  private static final Logger LOG = LoggerFactory.getLogger(CassandraInstallTool.class);
+
+  @Flag(name="kiji", usage="URI of the Cassandra Kiji instance to install.")
+  private String mKijiURIFlag = KConstants.DEFAULT_INSTANCE_URI;
+
+  @Flag(name="properties-file",
+      usage="The properties file, if any, to use instead of the defaults.")
+  private String mPropertiesFile = null;
+
+  /** URI of the Kiji instance to install. */
+  private KijiURI mKijiURI = null;
+
+  /** Unmodifiable empty map. */
+  private static final Map<String, String> EMPTY_MAP = Collections.emptyMap();
+
+  /** {@inheritDoc} */
+  @Override
+  public String getName() {
+    return "cassandra-install";
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public String getDescription() {
+    return "Install a Kiji instance onto a running Cassandra cluster.";
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public String getCategory() {
+    return "Admin";
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected void setup() throws Exception {
+    super.setup();
+    Preconditions.checkArgument((mKijiURIFlag != null) && !mKijiURIFlag.isEmpty(),
+        "Specify the Kiji instance to install with --kiji=kiji://cassandra-address/kiji-instance");
+    mKijiURI = KijiURI.newBuilder(mKijiURIFlag).build();
+    Preconditions.checkArgument(mKijiURI.getInstance() != null,
+        "Specify the Kiji instance to install with --kiji=kiji://cassandra-address/kiji-instance");
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected int run(List<String> nonFlagArgs) throws Exception {
+    getPrintStream().println("Creating Kiji instance: " + mKijiURI);
+    getPrintStream().println("Creating meta tables for Kiji instance in Cassandra...");
+    final Map<String, String> initialProperties = (null == mPropertiesFile)
+            ? EMPTY_MAP
+            : HBaseSystemTable.loadPropertiesFromFileToMap(mPropertiesFile);
+    try {
+      CassandraKijiInstaller.get().install(
+          mKijiURI,
+          initialProperties,
+          getConf());
+      getPrintStream().println("Successfully created Kiji instance: " + mKijiURI);
+      return SUCCESS;
+    } catch (KijiAlreadyExistsException kaee) {
+      getPrintStream().printf("Kiji instance '%s' already exists.%n", mKijiURI);
+      return FAILURE;
+    }
+  }
+
+  /**
+   * Program entry point.
+   *
+   * @param args The command-line arguments.
+   * @throws Exception If there is an error.
+   */
+  public static void main(String[] args) throws Exception {
+    System.exit(new KijiToolLauncher().run(new CassandraInstallTool(), args));
+  }
+}
